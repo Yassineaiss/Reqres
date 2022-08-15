54d7c54119f23fdad6556b2932fa3df765c7f859
abderrahimissaoui <aissaoui_abderrahim@hotmail.fr>
2022-08-15 19:19:59 +0200

    add users routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,18 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
-import { UsersResolveService } from './users-resolve.service';
+import { UsersComponent } from './users/users.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    UsersComponent,
 
   ],
   imports: [
     BrowserModule,
     AppRoutingModule, HttpClientModule
   ],
-  providers: [UsersResolveService],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
